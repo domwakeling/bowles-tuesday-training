b42f02931e83baf11934500ddcdb46dc49a36935
Dom Wakeling <domwakeling@gmail.com>
2021-12-16 13:44:47 +0000

    Xmas bookings

--- components/bookings.jsx
+++ components/bookings.jsx
@@ -46,10 +46,10 @@ const Bookings = () => {
         {' '}
         {ds[1]}
       </h2>
-      { ds[0] === '23032021' ? (
+      { ds[0] === '28122021' ? (
         <div className="alert-text">
-          <p>The centre is currently shut in accordance with Covid restrictions.</p>
-          <p>Bookings for Tuesday 30 March will open at midnight on the Tues/Weds 23/24.</p>
+          <p>There is no race club from Friday 24th &amp; Friday 31st December (inclusive).</p>
+          <p>Training will resume in January!</p>
         </div>
       ) : (
         <>
